e5210303de80259001ef2ad3037e27a465ceacd5
bethholzer <ebholzer@gmail.com>
2022-04-25 16:34:07 -0700

    p3 update

--- Projects/Project-03/sf-java.js
+++ Projects/Project-03/sf-java.js
@@ -1,55 +1,49 @@
-// BLOTTER
+/* Stuff for both galleries*/
+
+const images = ['blotter-1.png', 
+                'blotter-2.png', 
+                'blotter-3.png', 
+                'blotter-4.png', 
+                'blotter-5.png', 
+                'blotter-6.png', 
+                'blotter-7.png',];
+
+//just need image name, not the path, which is defined in setAttribute
+// you can create another array for different images
+
+var i = 0;
+
+/*Blotter Gallery*/
 
 let blotterGallery = document.querySelector('div.blotter-gallery');
 
-const emptyImages = document.querySelectorAll('img'); // doesn't necessarily have to be empty
+let blotterImages = document.querySelector('img.blotter');
 
-const images = ['assets/SF/blotter/blotter-1.png', 
-                'assets/SF/blotter/blotter-2.png', 
-                'assets/SF/blotter/blotter-3.png', 
-                'assets/SF/blotter/blotter-4.png', 
-                'assets/SF/blotter/blotter-5.png', 
-                'assets/SF/blotter/blotter-6.png', 
-                'assets/SF/blotter/blotter-7.png', ];
 
-var i=0;
-
-function populateImages(){
-    for(const image of emptyImages){
-        image.setAttribute('src', 'idk/'+images[i]);
-        i++;
-    }
-
-    if (i > 6){ i =0;}  // this number can change depending on how many images you have available
+function populateBlotterImages(){
+    blotterImages.setAttribute('src', 'assets/SF/blotter/'+images[i]);
+    i++;
+    if(i > 6){ i = 0;}
 }
 
-blotterGallery.addEventListener('click',populateImages);
+blotterGallery.addEventListener('click', populateBlotterImages);
 
-// EARTH
 
-console.log(emptyImages);
+/*Earth gallery*/
+
+const hamburger = ['WE-1.jpg', 
+                'WE-2.jpg', 
+                'WE-3.jpg', 
+                'WE-4.jpg', ];
 
 let earthGallery = document.querySelector('div.earth-gallery');
 
-const emptyImages = document.querySelectorAll('img'); 
+let earthImages = document.querySelector('img.earth'); 
 
-const images = ['assets/SF/whole-earth/WE-1.jpg', 
-                'assets/SF/whole-earth/WE-2.jpg', 
-                'assets/SF/whole-earth/WE-3.jpg', 
-                'assets/SF/whole-earth/WE-4.jpg', 
- ];
-
-var i=0;
-
-function populateImages(){
-    for(const image of emptyImages){
-        image.setAttribute('src', 'idk/'+images[i]);
+function populateEarthImages(){
+        earthImages.setAttribute('src', 'assets/SF/whole-earth/'+hamburger[i]);
         i++;
-    }
-
-    if (i > 3){ i =0;} 
+        if(i > 3){ i = 0;}
 }
 
-earthGallery.addEventListener('click',populateImages);
-
-console.log(emptyImages);
+earthGallery.addEventListener('click', populateEarthImages);
